0b9084f8e8279a84cd354da0b382302ef198c340
vitalybash <vetali04@mail.ru>
2020-12-07 17:48:08 +0300

    Удалил класс Field

--- main.py
+++ main.py
@@ -11,30 +11,6 @@ def load_image(img_path):
     return [image, image_rect]
 
 
-class Field:
-    def __init__(self, width=5, height=5, cell_size=15):
-        self.width, self.height = width, height
-        self.cell_size = cell_size
-        self.field = [[0 for j in range(width)] for i in range(height)]
-
-    def render(self):
-        for y in range(0, self.height * self.cell_size, self.cell_size):
-            for x in range(0, self.width * self.cell_size, self.cell_size):
-                pygame.draw.rect(screen, pygame.Color('white'),
-                                 (x, y, self.cell_size, self.cell_size), 1)
-
-    def get_cell(self, mouse_pos):
-        x, y = mouse_pos  # Распаковка координат клика
-        col = x // self.cell_size
-        row = y // self.cell_size
-
-        print(row, col)
-        print(len(self.field), len(self.field[0]))
-        if 0 <= row < len(self.field) and 0 <= col < len(self.field[0]):
-            return row, col
-        return None
-
-
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('OverSlept')
