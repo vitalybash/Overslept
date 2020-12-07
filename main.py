61cecb999504dc4421bca1a7a2f1ad41e746d9ae
AlkoKovad <sanya.dobriy.sosed@mail.ru>
2020-12-07 22:14:28 +0300

    каркас level_hub'а

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from developers_settings import *
 from interfaces.main_menu import MainMenu
+from interfaces.level_hub import LevelHub
 
 
 def load_image(img_path):
@@ -11,6 +12,30 @@ def load_image(img_path):
     return [image, image_rect]
 
 
+class Field:
+    def __init__(self, width=5, height=5, cell_size=15):
+        self.width, self.height = width, height
+        self.cell_size = cell_size
+        self.field = [[0 for j in range(width)] for i in range(height)]
+
+    def render(self):
+        for y in range(0, self.height * self.cell_size, self.cell_size):
+            for x in range(0, self.width * self.cell_size, self.cell_size):
+                pygame.draw.rect(screen, pygame.Color('white'),
+                                 (x, y, self.cell_size, self.cell_size), 1)
+
+    def get_cell(self, mouse_pos):
+        x, y = mouse_pos  # Распаковка координат клика
+        col = x // self.cell_size
+        row = y // self.cell_size
+
+        print(row, col)
+        print(len(self.field), len(self.field[0]))
+        if 0 <= row < len(self.field) and 0 <= col < len(self.field[0]):
+            return row, col
+        return None
+
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('OverSlept')
@@ -18,9 +43,10 @@ if __name__ == '__main__':
     screen.fill((0, 0, 0))
 
     # Экземпляр
-    MAIN_WND = MainMenu()
+#    MAIN_WND = MainMenu()
+    LEVEL_HUB = LevelHub()
     running = True
     while running:
-        running = MAIN_WND.render(screen)
+        running = LEVEL_HUB.render(screen)
     pygame.quit()
 
