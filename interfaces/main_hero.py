0e106cdd4c67c550ec4c60ff772c451cb0d2a2e6
AlkoKovad <sanya.dobriy.sosed@mail.ru>
2021-01-25 23:48:39 +0300

    main hero visual structure

---
+++ interfaces/main_hero.py
@@ -0,0 +1,27 @@
+import pygame as pg
+import sys
+
+import basic_functions as func
+from developers_settings import *
+from music.music import Music
+from interfaces.level import Level
+
+
+class MainHero:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def render_vibing(self, screen):
+        pass
+
+    def render_going(self, screen):
+        pass
+
+    def render_punching(self, screen):
+        pass
+
+    def render_hit(self, screen):
+        pass
